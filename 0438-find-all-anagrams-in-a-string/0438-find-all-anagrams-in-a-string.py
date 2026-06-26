dd847f3ccda947a752ac4abb32b9f40e667dc44a
VASANTH T N <psgktp18@gmail.com>
2026-06-26 08:50:56 +0530

    Time: 203 ms (14.32%), Space: 19.9 MB (41.37%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,25 @@
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        need = Counter(p)
+        window = Counter()
+
+        left = 0
+        right = 0
+        ans = []
+
+        while right < len(s):
+            window[s[right]] += 1
+
+            if right - left + 1 == len(p):
+                if window == need:
+                    ans.append(left)
+
+                window[s[left]] -= 1
+                if window[s[left]] == 0:
+                    del window[s[left]]
+                left += 1
+
+            right += 1
+
+        return ans
