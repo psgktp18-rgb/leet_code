00571c450435b958a4691b9e6d9da80022fa72b0
VASANTH T N <psgktp18@gmail.com>
2026-07-06 15:23:48 +0530

    Time: 11 ms (54.79%), Space: 19.4 MB (26.75%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,48 +1,32 @@
 class Solution:
-    def uniquePathsIII(self, grid):
+    def uniquePathsIII(self, g):
+        row = len(g)
+        cols = len(g[0])
+        e = 0
+        for i in range(row):
+            for j in range(cols):
+                if g[i][j] != -1:
+                    e += 1
+                if g[i][j] == 1:
+                    sr = i
+                    sc = j
+        def vasanth(r,c,rem):
+            if r<0 or r>=row or c<0 or c>=cols:
+                return False
+            if g[r][c] == -1:
+                return False
+            if g[r][c] == 2:
+                if rem == 1:   
+                    return True
+                return False
 
-        m = len(grid)
-        n = len(grid[0])
-
-        ans = 0
-        need = 0
-
-        sol = [[0] * n for _ in range(m)]
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] != -1:
-                    need += 1
-
-                if grid[i][j] == 1:
-                    sr, sc = i, j
-
-        def dfs(r, c, visited):
-            nonlocal ans
-
-            if r < 0 or c < 0 or r >= m or c >= n:
-                return
-
-            if grid[r][c] == -1:
-                return
-
-            if sol[r][c] == 1:
-                return
-
-            if grid[r][c] == 2:
-                if visited == need:
-                    ans += 1
-                return
-
-            sol[r][c] = 1
-
-            dfs(r + 1, c, visited + 1)
-            dfs(r - 1, c, visited + 1)
-            dfs(r, c + 1, visited + 1)
-            dfs(r, c - 1, visited + 1)
-
-            sol[r][c] = 0
-
-        dfs(sr, sc, 1)
-
-        return ans
+            temp = g[r][c]
+            g[r][c] = -1
+            p = 0
+            p += vasanth(r+1,c,rem-1)
+            p += vasanth(r-1,c,rem-1)
+            p += vasanth(r,c+1,rem-1)
+            p += vasanth(r,c-1,rem-1)
+            g[r][c] = temp
+            return p
+        return vasanth(sr,sc,e)
