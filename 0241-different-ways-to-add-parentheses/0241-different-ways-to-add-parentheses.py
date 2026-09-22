91b887cd7d4fda94fa909dc5a6a5d61e15d6bb0a
VASANTH T N <psgktp18@gmail.com>
2026-09-22 21:00:35 +0530

    Time: 0 ms (100%), Space: 19.4 MB (31.83%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,23 +1,43 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-        res = []
-        # ans = []
-        for i in range(len(expression)):
-            oper = expression[i]
-            if oper == "+" or oper == "-" or oper == "*":
-                sub_str1 = expression[0 : i]
-                sub_str2 = expression[i + 1 : ]
-                s1 = self.diffWaysToCompute(sub_str1)
-                s2 = self.diffWaysToCompute(sub_str2)
-                for i in s1:
-                    for j in s2:
-                        if oper == "*":
-                            res.append(int(i) * int(j))
-                        if oper == "+":
-                            res.append(int(i) + int(j))
-                        if oper == "-":
-                            res.append(int(i) - int(j))
-        if len(res) == 0:
-            res.append(int(expression))
-        # print(res)
-        return res
+    def isNum(self, c):
+        return c >= '0' and c <= '9'
+
+    def solve(self, i, j, exp):
+
+        if i == j:
+            return [int(exp[i])]
+
+        if j - i <= 1 and self.isNum(exp[i]) and self.isNum(exp[j]):
+            return [int(exp[i]) * 10 + int(exp[j])]
+
+        if (i, j) in self.dp:
+            return self.dp[(i, j)]
+
+        posAns = []
+
+        for k in range(i + 1, j):
+
+            if self.isNum(exp[k]):
+                continue
+
+            leftSolution = self.solve(i, k - 1, exp)
+            rightSolution = self.solve(k + 1, j, exp)
+
+            for x in leftSolution:
+                for y in rightSolution:
+
+                    if exp[k] == '*':
+                        posAns.append(x * y)
+
+                    elif exp[k] == '-':
+                        posAns.append(x - y)
+
+                    elif exp[k] == '+':
+                        posAns.append(x + y)
+
+        self.dp[(i, j)] = posAns
+        return posAns
+
+    def diffWaysToCompute(self, expression):
+        self.dp = {}
+        return self.solve(0, len(expression) - 1, expression)
